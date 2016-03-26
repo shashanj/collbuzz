77b556e9a41bacf095c5623e4e86ec482b40f782
shashank Singh <shashanks.1903@gmail.com>
2016-03-27 02:58:52 +0530

    db designed

--- models/2.py
+++ models/2.py
@@ -1,45 +1,100 @@
 # Here define general models apart from user and few essential models
 
 db.define_table('sellinng_item',
-	Field('name'),
-	Field('seller'),
-	Field('category'),
-	Field('sub_cat'),
+	Field('name','string'),
+	Field('seller','reference user'),
+	Field('category','string'),
+	Field('sub_cat','string'),
 	Field('bill_av','boolean'), # weather bill is availaible or not
-	Field('min_bid'),
-	Field('max_bid'),
-	Field('current_bid'),
-	Field('current_bid_user'),
-	Field('descript'),
-	Field('tags'),
-	Field('orig_amnt'),
-	Field('mobile'),
-	Field('expires_on'),
-	Field('pr_amnt'),
-	Field('pr_till'),
-	Field('delivery_type'),
-	Field('max_no_bids'),
+	Field('min_bid','double'),
+	Field('max_bid','double'),
+	Field('current_bid','double'),
+	Field('current_bid_user','reference user'),
+	Field('descript','text'),
+	Field('tags','string'),
+	Field('orig_amnt','double'),
+	Field('mobile','string',length=10),
+	Field('expires_on','date'),
+	Field('pr_amnt','double'),
+	Field('pr_till','date'),
+	Field('delivery_type','string'),
+	Field('max_no_bids','integer'),
 	Field('view_count','integer',default=0),
 	Field('like_count','integer',default=0),
 	Field('bid_count','integer',default=0),
-	Field('type'), # Premium, Hot etc... 
+	Field('type','string'), # Premium, Hot etc... 
 	Field('status','integer'), # Item sold status- can be 0,1,2,3, etc for sold, hot , deleted , etc.. 
-	Field('image'), # Will be string, storing multiple image names only 
-	Field('video'),
-	Field('flag'),
+	Field('image','string'), # Will be string, storing multiple image names only 
+	Field('video','string'),
+	Field('flag','integer'),
 	Field('need_rev'), # Needs review
-	Field('lati'),
-	Field('longi'),
-	Field('autolist'),
-	Field('list_fee'),
-	Field('trans_fee'),
+	Field('lati','string'),
+	Field('longi','string'),
+	Field('autolist','boolean'),
+	Field('list_fee','string'),
+	Field('trans_fee','string'),
 	format="%(name)s"
 )
 
 db.define_table('item_bid',
-	Field('bidder'),
-	Field('bid_item'),
-	Field('amount'),
-	Field('status'), 	#status of bid, 0,1,2, etc.. it's seen, accepted,rejected,outbidded etc...
+	Field('bidder','reference user'),
+	Field('bid_item','reference sellinng_item'),
+	Field('amount','double'),
+	Field('status','string'), 	#status of bid, 0,1,2, etc.. it's seen, accepted,rejected,outbidded etc...
 )
 
+db.define_table('tasks',
+	Field('body','text'), # sort of eligilbility
+)
+
+db.define_table('credits',
+	Field('name','string'),
+	Field('reward','integer'), # amount of reward
+	Field('tasks','list:reference tasks'), # many to one relation with tasks
+	Field('started_on','datetime'),
+	Field('ends_on','datetime'),
+	Field('active','boolean'), # instead of delete use to deactivate the credit in between
+	Field('timestamp','datetime',default=request.now,writable=False,readable=False),
+)
+
+db.define_table('user_credits', # mapping user for credits they want to score
+	Field('user','reference user'),
+	Field('credit','reference credits'),
+	Field('status', 'string'), # complete, in progres , 
+	format="%(name)s",
+)
+
+db.define_table('item_comment',
+	Field('comment','text'),
+	Field('item','reference sellinng_item'),
+	Field('comment_by','reference user'),
+	Field('replies','list:reference comment_reply'),
+	Field('like','integer',default=0),
+	Field('dislike','integer',default=0),
+	Field('timestamp','datetime',default=request.now),
+)
+
+db.define_table('comment_reply',
+	Field('reply','text'),
+	Field('item','reference sellinng_item'),
+	Field('reply_by','reference user'),
+	Field('like','integer',default=0),
+	Field('dislike','integer',default=0),
+	Field('timestamp','datetime',default=request.now),
+)
+
+db.define_table('flag_reason',
+	Field('body','string'),
+)
+
+db.define_table('flag_ad',
+	Field('item','reference sellinng_item'),
+	Field('flagged_by','reference user'),
+	Field('reason','reference flag_reason'),
+	Field('timestamp','datetime',default=request.now),
+)
+
+db.define_table('shortlist',
+	Field('user','reference user'),
+	Field('item', 'list:reference sellinng_item'),
+)
